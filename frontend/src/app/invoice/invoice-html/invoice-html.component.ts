14f8e4d46e4024bf9435a55ba3c87fe6c9e73935
abrarShariar <shariarabrar@gmail.com>
2017-05-23 01:30:15 +0600

    minor changes

--- frontend/src/app/invoice/invoice-html/invoice-html.component.ts
+++ frontend/src/app/invoice/invoice-html/invoice-html.component.ts
@@ -45,7 +45,7 @@ export class InvoiceHtmlComponent implements OnInit {
   }
 
   downloadPDF() {
-    html2canvas(document.getElementById('invoice_box'), {
+    html2canvas(document.getElementById('invoice_body'), {
       onrendered: (canvas) => {
         var imgData = canvas.toDataURL("image/jpeg", 1.0);
         var pdf = new jsPDF("p", "mm", "a4");
@@ -80,9 +80,11 @@ export class InvoiceHtmlComponent implements OnInit {
                       this.invoice.amount_due = this.invoice.amount_due - data['amount'];
                     });
                   }
+                  if(this.invoice.status == 'Paid'){
+                    this.invoice.amount_due = 0;
+                  }
                   this.finalTotal = this.invoice.amount_due - this.invoice.discount;
-                  this.finalTotalWords = this.inWords(this.finalTotal);
-                  console.log(this.finalTotalWords);
+                  this.finalTotalWords = this.numberToEnglish(this.invoice.total,'');
                 }
               )
           });
@@ -108,23 +110,94 @@ export class InvoiceHtmlComponent implements OnInit {
       )
   }
 
-  private a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-  private b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+  numberToEnglish(n, custom_join_character) {
 
-  inWords (num) {
-  if ((num = num.toString()).length > 9) return 'overflow';
-  let n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-  if (!n) return;
-  var str = '';
-  str += (n[1] != 0) ? (this.a[Number(n[1])] || this.b[n[1][0]] + ' ' + this.a[n[1][1]]) + 'crore ' : '';
-  str += (n[2] != 0) ? (this.a[Number(n[2])] || this.b[n[2][0]] + ' ' + this.a[n[2][1]]) + 'lakh ' : '';
-  str += (n[3] != 0) ? (this.a[Number(n[3])] || this.b[n[3][0]] + ' ' + this.a[n[3][1]]) + 'thousand ' : '';
-  str += (n[4] != 0) ? (this.a[Number(n[4])] || this.b[n[4][0]] + ' ' + this.a[n[4][1]]) + 'hundred ' : '';
-  str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (this.a[Number(n[5])] || this.b[n[5][0]] + ' ' + this.a[n[5][1]]) + 'only ' : '';
-  return str;
-}
+    var string = n.toString(),
+      units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words;
 
+    var and = custom_join_character || 'and';
 
+    /* Is number zero? */
+    if (parseInt(string) === 0) {
+      return 'zero';
+    }
+
+    /* Array of units as words */
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    /* Array of tens as words */
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    /* Array of scales as words */
+    scales = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion'];
+
+    /* Split user arguemnt into 3 digit chunks from right to left */
+    start = string.length;
+    chunks = [];
+    while (start > 0) {
+      end = start;
+      chunks.push(string.slice((start = Math.max(0, start - 3)), end));
+    }
+
+    /* Check if function has enough scale words to be able to stringify the user argument */
+    chunksLen = chunks.length;
+    if (chunksLen > scales.length) {
+      return '';
+    }
+
+    /* Stringify each integer in each chunk */
+    words = [];
+    for (i = 0; i < chunksLen; i++) {
+
+      chunk = parseInt(chunks[i]);
+
+      if (chunk) {
+
+        /* Split chunk into array of individual integers */
+        ints = chunks[i].split('').reverse().map(parseFloat);
+
+        /* If tens integer is 1, i.e. 10, then add 10 to units integer */
+        if (ints[1] === 1) {
+          ints[0] += 10;
+        }
+
+        /* Add scale word if chunk is not zero and array item exists */
+        if ((word = scales[i])) {
+          words.push(word);
+        }
+
+        /* Add unit word if array item exists */
+        if ((word = units[ints[0]])) {
+          words.push(word);
+        }
+
+        /* Add tens word if array item exists */
+        if ((word = tens[ints[1]])) {
+          words.push(word);
+        }
+
+        /* Add 'and' string after units or tens integer if: */
+        if (ints[0] || ints[1]) {
+
+          /* Chunk has a hundreds integer or chunk is the first of multiple chunks */
+          if (ints[2] || !i && chunksLen) {
+            words.push(and);
+          }
+
+        }
+
+        /* Add hundreds word if array item exists */
+        if ((word = units[ints[2]])) {
+          words.push(word + ' hundred');
+        }
+
+      }
+
+    }
+
+    return words.reverse().join(' ');
+
+  }
 
 
 }
